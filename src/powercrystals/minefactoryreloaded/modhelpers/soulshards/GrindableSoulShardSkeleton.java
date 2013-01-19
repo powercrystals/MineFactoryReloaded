58b499cfb7504cef49d120bfb67901d3d94a302e
powercrystals <powcrystals@gmail.com>
2013-01-18 22:45:23 -0500

    Soul Shards support added

---
+++ src/powercrystals/minefactoryreloaded/modhelpers/soulshards/GrindableSoulShardSkeleton.java
@@ -0,0 +1,19 @@
+package powercrystals.minefactoryreloaded.modhelpers.soulshards;
+
+import powercrystals.minefactoryreloaded.farmables.GrindableSkeleton;
+
+public class GrindableSoulShardSkeleton extends GrindableSkeleton
+{
+	private Class<?> _soulShardSkeletonClass;
+	
+	public GrindableSoulShardSkeleton(Class<?> soulShardSkeletonClass)
+	{
+		_soulShardSkeletonClass = soulShardSkeletonClass;
+	}
+	
+	@Override
+	public Class<?> getGrindableEntity()
+	{
+		return _soulShardSkeletonClass;
+	}
+}
---
+++ src/powercrystals/minefactoryreloaded/modhelpers/soulshards/SoulShards.java
@@ -0,0 +1,41 @@
+package powercrystals.minefactoryreloaded.modhelpers.soulshards;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import powercrystals.minefactoryreloaded.MineFactoryReloadedCore;
+import powercrystals.minefactoryreloaded.api.FarmingRegistry;
+import powercrystals.minefactoryreloaded.farmables.GrindableStandard;
+import cpw.mods.fml.common.FMLLog;
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid = "MFReloaded|CompatSoulShards", name = "MFR Compat: Soul Shards", version = MineFactoryReloadedCore.version, dependencies = "after:MFReloaded;after:SoulShards")
+@NetworkMod(clientSideRequired = false, serverSideRequired = false)
+public class SoulShards
+{
+	@Init
+	public static void load(FMLInitializationEvent e)
+	{
+		if(!Loader.isModLoaded("SoulShards"))
+		{
+			FMLLog.warning("SoulShards missing - MFR SoulShards Compat not loading");
+			return;
+		}
+		try
+		{
+			Class<?> soulShardBlaze = Class.forName("com.shadwdrgn.soulshards.EntitySpawnedBlaze");
+			Class<?> soulShardSkeleton = Class.forName("com.shadwdrgn.soulshards.EntitySpawnedSkeleton");
+			Class<?> soulShardZombie = Class.forName("com.shadwdrgn.soulshards.EntitySpawnedZombie");
+			FarmingRegistry.registerGrindable(new GrindableStandard(soulShardBlaze, new ItemStack(Item.blazeRod)));
+			FarmingRegistry.registerGrindable(new GrindableSoulShardSkeleton(soulShardSkeleton));
+			FarmingRegistry.registerGrindable(new GrindableStandard(soulShardZombie, new ItemStack(Item.rottenFlesh)));
+		}
+		catch (Exception x)
+		{
+			x.printStackTrace();
+		}
+	}
+}
