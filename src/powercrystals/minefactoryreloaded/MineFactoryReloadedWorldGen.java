6c9178d0c88eb24b5271c4fd8dbf4c3b2ea6b650
Ross Swartz <ross.swartz@gmail.com>
2013-02-06 14:50:43 -0500

    Change rubber tree spawning to support multiple biomes

--- src/powercrystals/minefactoryreloaded/MineFactoryReloadedWorldGen.java
+++ src/powercrystals/minefactoryreloaded/MineFactoryReloadedWorldGen.java
@@ -14,12 +14,16 @@ public class MineFactoryReloadedWorldGen implements IWorldGenerator
 	@Override
 	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider)
 	{
+		String[] allowedBiomes = {"Swamplands", "Forest", "Taiga", "TaigaHills", "Jungle", "JungleHills"};
 		BiomeGenBase b = world.getBiomeGenForCoords(chunkX, chunkZ);
-		if(b.biomeName.toLowerCase().contains("swamp"))
+		for (String s : allowedBiomes)
 		{
-			if(random.nextInt(100) < 40)
+			if(b.biomeName.equals(s))
 			{
-				new WorldGenRubberTree().generate(world, random, chunkX * 16 + random.nextInt(16), random.nextInt(3) + 4, chunkZ * 16 + random.nextInt(16));
+				if(random.nextInt(100) < 40)
+				{
+					new WorldGenRubberTree().generate(world, random, chunkX * 16 + random.nextInt(16), random.nextInt(3) + 4, chunkZ * 16 + random.nextInt(16));
+				}
 			}
 		}
 	}
