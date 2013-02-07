31dadb3743acc57031f7c289105dde56cc385fbd
Ross Swartz <ross.swartz@gmail.com>
2013-02-06 19:49:27 -0500

    Fixed those pesky trees. "And the lord said: LET THERE BE MFR RUBBER TREES"

--- src/powercrystals/minefactoryreloaded/MineFactoryReloadedWorldGen.java
+++ src/powercrystals/minefactoryreloaded/MineFactoryReloadedWorldGen.java
@@ -14,15 +14,17 @@ public class MineFactoryReloadedWorldGen implements IWorldGenerator
 	@Override
 	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider)
 	{
-		String[] allowedBiomes = {"Swamplands", "Forest", "Taiga", "TaigaHills", "Jungle", "JungleHills"};
-		BiomeGenBase b = world.getBiomeGenForCoords(chunkX, chunkZ);
+		String[] allowedBiomes = {"Swampland", "Forest", "Taiga", "TaigaHills", "Jungle", "JungleHills"};
+		int x = chunkX * 16 + random.nextInt(16);
+		int z = chunkZ * 16 + random.nextInt(16);
+		BiomeGenBase b = world.getBiomeGenForCoords(x, z);
 		for (String s : allowedBiomes)
 		{
 			if(b.biomeName.equals(s))
 			{
 				if(random.nextInt(100) < 40)
 				{
-					new WorldGenRubberTree().generate(world, random, chunkX * 16 + random.nextInt(16), random.nextInt(3) + 4, chunkZ * 16 + random.nextInt(16));
+					new WorldGenRubberTree().generate(world, random, x, random.nextInt(3) + 4, z);
 				}
 			}
 		}
