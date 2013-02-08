c05655303781f2d532fec29d7ddf436d99b383f4
powercrystals <powcrystals@gmail.com>
2013-02-07 19:07:37 -0500

    Moved NEI class to its own package

--- src/powercrystals/minefactoryreloaded/modhelpers/nei/NEIMFRConfig.java
+++ src/powercrystals/minefactoryreloaded/modhelpers/nei/NEIMFRConfig.java
@@ -1,4 +1,4 @@
-package powercrystals.minefactoryreloaded.modhelpers;
+package powercrystals.minefactoryreloaded.modhelpers.nei;
 
 import powercrystals.minefactoryreloaded.MineFactoryReloadedCore;
 import codechicken.nei.MultiItemRange;
