789bdd7866f54b6ecfd1b9bfe1107a61ec145fdd
powercrystals <powcrystals@gmail.com>
2013-02-17 22:04:39 -0500

    Changed interface

--- src/powercrystals/minefactoryreloaded/api/ISafariNetHandler.java
+++ src/powercrystals/minefactoryreloaded/api/ISafariNetHandler.java
@@ -1,15 +1,19 @@
 package powercrystals.minefactoryreloaded.api;
 
+import java.util.List;
+
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 
 public interface ISafariNetHandler
 {
 	public Class<?> validFor();
 	
-	public void onCapture(ItemStack safariNetStack, Entity capturedEntity);
+	public void onCapture(NBTTagCompound safariNetTag, Entity capturedEntity);
 	
-	public void onRelease(ItemStack safariNetStack, Entity capturedEntity);
+	public void onRelease(NBTTagCompound safariNetTag, Entity capturedEntity);
 
-	public void addInformation(ItemStack safariNetStack);
+	public void addInformation(ItemStack safariNetStack, EntityPlayer player, List infoList, boolean advancedTooltips);
 }
