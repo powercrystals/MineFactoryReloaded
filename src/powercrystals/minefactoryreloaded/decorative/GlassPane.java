e3d64da10ba9f19b73b76728bad1c4fd9ff67fc8
powercrystals <powcrystals@gmail.com>
2013-02-25 21:36:42 -0500

    Use MFR's pane class to replace vanilla

--- src/powercrystals/minefactoryreloaded/decorative/GlassPane.java
+++ src/powercrystals/minefactoryreloaded/decorative/GlassPane.java
@@ -1,24 +1,25 @@
 package powercrystals.minefactoryreloaded.decorative;
 
-import powercrystals.minefactoryreloaded.MineFactoryReloadedCore;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockPane;
-import net.minecraft.block.material.Material;
 
-public class GlassPane extends BlockPane
+public class GlassPane extends BlockFactoryGlassPane
 {
 	public GlassPane()
 	{
-		super(102, 49, 148, Material.glass, false);
+		super(102, 49, 148);
 		setHardness(0.3F);
 		setStepSound(soundGlassFootstep);
 		setBlockName("thinGlass");
 	}
 	
 	@Override
-    public boolean canThisPaneConnectToThisBlockID(int blockId)
-    {
-        return Block.opaqueCubeLookup[blockId] || blockId == this.blockID || blockId == Block.glass.blockID
-        		|| blockId == MineFactoryReloadedCore.factoryGlassBlock.blockID || blockId == MineFactoryReloadedCore.factoryGlassPaneBlock.blockID;
-    }
+	public int getBlockSideTextureFromMetadata(int meta)
+	{
+		return getSideTextureIndex();
+	}
+	
+	@Override
+	public String getTextureFile()
+	{
+		return "/terrain.png";
+	}
 }
