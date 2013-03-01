3dc81097521ef33e8e3dd1ad9c634ed0263054b6
powercrystals <powcrystals@gmail.com>
2013-02-28 22:26:59 -0500

    Very noisy tree debugging

--- src/powercrystals/minefactoryreloaded/world/MineFactoryReloadedWorldGen.java
+++ src/powercrystals/minefactoryreloaded/world/MineFactoryReloadedWorldGen.java
@@ -18,14 +18,19 @@ public class MineFactoryReloadedWorldGen implements IWorldGenerator
 		int x = chunkX * 16 + random.nextInt(16);
 		int z = chunkZ * 16 + random.nextInt(16);
 		BiomeGenBase b = world.getBiomeGenForCoords(x, z);
+		
+		System.out.println("Our biome: " + b.biomeName);
 		for(String s : MFRRegistry.getRubberTreeBiomes())
 		{
-			if(b.biomeName.equals(s))
+			System.out.print(s);
+		}
+		System.out.println();
+		
+		if(MFRRegistry.getRubberTreeBiomes().contains(b.biomeName))
+		{
+			if(random.nextInt(100) < 40)
 			{
-				if(random.nextInt(100) < 40)
-				{
-					new WorldGenRubberTree().generate(world, random, x, random.nextInt(3) + 4, z);
-				}
+				new WorldGenRubberTree().generate(world, random, x, random.nextInt(3) + 4, z);
 			}
 		}
 	}
