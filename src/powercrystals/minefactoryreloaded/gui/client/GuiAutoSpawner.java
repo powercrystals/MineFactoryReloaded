0cecd8b300c5263d20208720f4e6d044daceb2d5
powercrystals <powcrystals@gmail.com>
2013-03-03 18:02:13 -0500

    Slight button adjustments

--- src/powercrystals/minefactoryreloaded/gui/client/GuiAutoSpawner.java
+++ src/powercrystals/minefactoryreloaded/gui/client/GuiAutoSpawner.java
@@ -26,7 +26,7 @@ public class GuiAutoSpawner extends GuiFactoryPowered
 		int xOffset = (this.width - this.xSize) / 2;
 		int yOffset = (this.height - this.ySize) / 2;
 		
-		_toggle = new GuiButton(1, xOffset + 7, yOffset + 40, 110, 20, "Spawn Exact Copy: ");
+		_toggle = new GuiButton(1, xOffset + 7, yOffset + 44, 115, 20, "Spawn Exact Copy: ");
 		
 		controlList.add(_toggle);
 	}
