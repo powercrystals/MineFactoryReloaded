742881438848ad58a58f3c491ffcfcb0388e7a96
powercrystals <powcrystals@gmail.com>
2013-03-03 22:58:05 -0500

    Glowstone bricks emit light

--- src/powercrystals/minefactoryreloaded/decorative/BlockFactoryDecorativeBricks.java
+++ src/powercrystals/minefactoryreloaded/decorative/BlockFactoryDecorativeBricks.java
@@ -6,6 +6,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.world.IBlockAccess;
 
 public class BlockFactoryDecorativeBricks extends Block
 {
@@ -23,13 +24,20 @@ public class BlockFactoryDecorativeBricks extends Block
 	@SideOnly(Side.CLIENT)
 	public int getRenderBlockPass()
 	{
-		return 1;
+		return 0;
 	}
 	
 	@Override
 	public int damageDropped(int meta)
 	{
 		return meta;
+	}
+	
+	@Override
+	public int getLightValue(IBlockAccess world, int x, int y, int z)
+	{
+		int meta = world.getBlockMetadata(x, y, z);
+		return meta == 0 ? 15 : 0;
 	}
 	
 	@Override
