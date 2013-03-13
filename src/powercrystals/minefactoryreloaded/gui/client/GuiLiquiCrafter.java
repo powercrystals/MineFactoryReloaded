48ec00d6acf647690097f795cc6f8bf27757c5ea
powercrystals <powcrystals@gmail.com>
2013-03-13 00:10:48 -0400

    Partial Liquicrafter fix

--- src/powercrystals/minefactoryreloaded/gui/client/GuiLiquiCrafter.java
+++ src/powercrystals/minefactoryreloaded/gui/client/GuiLiquiCrafter.java
@@ -3,8 +3,11 @@ package powercrystals.minefactoryreloaded.gui.client;
 import org.lwjgl.opengl.GL11;
 
 import powercrystals.minefactoryreloaded.MineFactoryReloadedCore;
-import powercrystals.minefactoryreloaded.gui.container.ContainerFactoryInventory;
+import powercrystals.minefactoryreloaded.gui.container.ContainerLiquiCrafter;
 import powercrystals.minefactoryreloaded.processing.TileEntityLiquiCrafter;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.ForgeHooksClient;
 import net.minecraftforge.common.ForgeDirection;
 import net.minecraftforge.liquids.LiquidStack;
 
@@ -12,11 +15,12 @@ public class GuiLiquiCrafter extends GuiFactoryInventory
 {
 	private TileEntityLiquiCrafter _crafter;
 
-	public GuiLiquiCrafter(ContainerFactoryInventory container, TileEntityLiquiCrafter router)
+	public GuiLiquiCrafter(ContainerLiquiCrafter container, TileEntityLiquiCrafter router)
 	{
 		super(container, router);
 		_crafter = router;
-		ySize = 256;
+		xSize = 231;
+		ySize = 214;
 	}
 	
 	@Override
@@ -41,6 +45,67 @@ public class GuiLiquiCrafter extends GuiFactoryInventory
 		for(int i = 0; i < 8; i++)
 		{
 			this.drawTexturedModalRect(-50 + (i % 3 * 18), 10 + (i / 3 * 35), 232, 0, 16, 33);
+		}
+	}
+
+	@Override
+	protected void drawGuiContainerBackgroundLayer(float gameTicks, int mouseX, int mouseY)
+	{
+		int texture = mc.renderEngine.getTexture(MineFactoryReloadedCore.guiFolder + _tileEntity.getGuiBackground());
+
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		this.mc.renderEngine.bindTexture(texture);
+		int x = (width - xSize) / 2 - 56;
+		int y = (height - ySize) / 2;
+		this.drawTexturedModalRect(x, y, 0, 0, xSize, ySize);
+	}
+	
+	@Override
+	protected void drawTank(int xOffset, int yOffset, int liquidId, int liquidMeta, int level)
+	{
+		int liquidTexture = 0;
+
+		if (liquidId <= 0)
+		{
+			return;
+		}
+		if (liquidId < Block.blocksList.length && Block.blocksList[liquidId] != null)
+		{
+			ForgeHooksClient.bindTexture(Block.blocksList[liquidId].getTextureFile(), 0);
+			liquidTexture = Block.blocksList[liquidId].blockIndexInTexture;
+		}
+		else if	(Item.itemsList[liquidId] != null)
+		{
+			ForgeHooksClient.bindTexture(Item.itemsList[liquidId].getTextureFile(), 0);
+			liquidTexture = Item.itemsList[liquidId].getIconFromDamage(liquidMeta);
+		}
+		else
+		{
+			return;
+		}
+
+		int liquidTexY = liquidTexture / 16;
+		int liquidTexX = liquidTexture - liquidTexY * 16;
+
+		int vertOffset = 0;
+
+		while(level > 0)
+		{
+			int x = 0;
+	
+			if (level > 16)
+			{
+				x = 16;
+				level -= 16;
+			}
+			else
+			{
+				x = level;
+				level = 0;
+			}
+	
+			drawTexturedModalRect(xOffset, yOffset - x - vertOffset, liquidTexX * 16, liquidTexY * 16 + (16 - x), 16, 16 - (16 - x));
+			vertOffset = vertOffset + 16;
 		}
 	}
 }
