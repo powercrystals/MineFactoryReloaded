e375ed75ab5813bc1766db6c0be7b4b3e41fd17d
Emy <emy@fake.email>
2013-03-13 18:15:57 -0600

    2.3.2b1

--- src/powercrystals/minefactoryreloaded/MineFactoryReloadedCore.java
+++ src/powercrystals/minefactoryreloaded/MineFactoryReloadedCore.java
@@ -125,7 +125,7 @@ public class MineFactoryReloadedCore implements IUpdateableMod
 	public static IMFRProxy proxy;
 	
 	public static final String modId = "MFReloaded";
-	public static final String version = "1.4.6R2.3.1";
+	public static final String version = "1.4.6R2.3.2B1";
 	public static final String modName = "Minefactory Reloaded";
 	
 	private static final String textureFolder = "/powercrystals/minefactoryreloaded/textures/";
