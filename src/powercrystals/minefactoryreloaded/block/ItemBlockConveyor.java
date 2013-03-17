426e6d497f9cc226dd4bb3d8fdb77a5396ba66a3
powercrystals <powcrystals@gmail.com>
2013-03-16 23:28:52 -0400

    Added conveyor tooltip for hammer use

--- src/powercrystals/minefactoryreloaded/block/ItemBlockConveyor.java
+++ src/powercrystals/minefactoryreloaded/block/ItemBlockConveyor.java
@@ -1,5 +1,9 @@
 package powercrystals.minefactoryreloaded.block;
 
+import java.util.List;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
@@ -13,6 +17,14 @@ public class ItemBlockConveyor extends ItemBlockFactory
 		setMaxDamage(0);
 		setHasSubtypes(true);
 		setNames(new String[] { "white", "orange", "magenta", "lightblue", "yellow", "lime", "pink", "gray", "lightgray", "cyan", "purple", "blue", "brown", "green", "red", "black", "default" });
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, EntityPlayer player, List infoList, boolean advancedTooltips)
+	{
+		infoList.add("Use Sledgehammer to rotate or slope");
 	}
 	
 	@Override
