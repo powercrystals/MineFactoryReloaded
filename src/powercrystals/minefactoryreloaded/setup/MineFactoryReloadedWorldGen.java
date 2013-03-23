336f89b94210ef96f2ff18f646deab3af1299ac7
powercrystals <powcrystals@gmail.com>
2013-03-23 18:06:16 -0400

    Added sludge and sewage lakes

--- src/powercrystals/minefactoryreloaded/setup/MineFactoryReloadedWorldGen.java
+++ src/powercrystals/minefactoryreloaded/setup/MineFactoryReloadedWorldGen.java
@@ -3,11 +3,13 @@ package powercrystals.minefactoryreloaded.setup;
 import java.util.Random;
 
 import powercrystals.minefactoryreloaded.MFRRegistry;
+import powercrystals.minefactoryreloaded.MineFactoryReloadedCore;
 
 
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.feature.WorldGenLakes;
 import cpw.mods.fml.common.IWorldGenerator;
 
 public class MineFactoryReloadedWorldGen implements IWorldGenerator
@@ -25,6 +27,24 @@ public class MineFactoryReloadedWorldGen implements IWorldGenerator
 			{
 				new WorldGenRubberTree().generate(world, random, x, random.nextInt(3) + 4, z);
 			}
+		}
+		
+		if(random.nextInt(8) == 0)
+		{
+			int lakeX = x - 8 + random.nextInt(16);
+			int lakeY = random.nextInt(128);
+			int lakeZ = z - 8 + random.nextInt(16);
+			new WorldGenLakes(MineFactoryReloadedCore.sludgeStill.blockID).generate(world, random, lakeX, lakeY, lakeZ);
+			System.out.println("Generating sludge lake at " + lakeX + "," + lakeY + "," + lakeZ);
+		}
+		
+		if(random.nextInt(8) == 0)
+		{
+			int lakeX = x - 8 + random.nextInt(16);
+			int lakeY = random.nextInt(128);
+			int lakeZ = z - 8 + random.nextInt(16);
+			new WorldGenLakes(MineFactoryReloadedCore.sewageStill.blockID).generate(world, random, lakeX, lakeY, lakeZ);
+			System.out.println("Generating sewage lake at " + lakeX + "," + lakeY + "," + lakeZ);
 		}
 	}
 }
