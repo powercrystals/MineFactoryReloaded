0d5f039ccef1b73311463ee4e2b8d3fd87c4a1e0
powercrystals <powcrystals@gmail.com>
2013-03-25 21:38:23 -0400

    Fix autoenchanter misbehaving with shiftclick

--- src/powercrystals/minefactoryreloaded/gui/container/ContainerFactoryInventory.java
+++ src/powercrystals/minefactoryreloaded/gui/container/ContainerFactoryInventory.java
@@ -145,4 +145,96 @@ public class ContainerFactoryInventory extends Container
 			addSlotToContainer(new Slot(inventoryPlayer, i, 8 + i * 18, getPlayerInventoryVerticalOffset() + 58));
 		}
 	}
+	
+	@Override
+	protected boolean mergeItemStack(ItemStack stack, int slotStart, int slotRange, boolean reverse)
+	{
+		boolean successful = false;
+		int slotIndex = slotStart;
+		int maxStack = Math.min(stack.getMaxStackSize(), _te.getInventoryStackLimit());
+
+		if(reverse)
+		{
+			slotIndex = slotRange - 1;
+		}
+
+		Slot slot;
+		ItemStack existingStack;
+
+		if(stack.isStackable())
+		{
+			while(stack.stackSize > 0 && (!reverse && slotIndex < slotRange || reverse && slotIndex >= slotStart))
+			{
+				slot = (Slot)this.inventorySlots.get(slotIndex);
+				existingStack = slot.getStack();
+
+				if(existingStack != null && existingStack.itemID == stack.itemID && (!stack.getHasSubtypes() || stack.getItemDamage() == existingStack.getItemDamage()) && ItemStack.areItemStackTagsEqual(stack, existingStack))
+				{
+					int existingSize = existingStack.stackSize + stack.stackSize;
+
+					if(existingSize <= maxStack)
+					{
+						stack.stackSize = 0;
+						existingStack.stackSize = existingSize;
+						slot.onSlotChanged();
+						successful = true;
+					}
+					else if (existingStack.stackSize < maxStack)
+					{
+						stack.stackSize -= maxStack - existingStack.stackSize;
+						existingStack.stackSize = maxStack;
+						slot.onSlotChanged();
+						successful = true;
+					}
+				}
+
+				if(reverse)
+				{
+					--slotIndex;
+				}
+				else
+				{
+					++slotIndex;
+				}
+			}
+		}
+
+		if(stack.stackSize > 0)
+		{
+			if(reverse)
+			{
+				slotIndex = slotRange - 1;
+			}
+			else
+			{
+				slotIndex = slotStart;
+			}
+
+			while(!reverse && slotIndex < slotRange || reverse && slotIndex >= slotStart)
+			{
+				slot = (Slot)this.inventorySlots.get(slotIndex);
+				existingStack = slot.getStack();
+
+				if(existingStack == null)
+				{
+					slot.putStack(stack.copy());
+					slot.onSlotChanged();
+					stack.stackSize = 0;
+					successful = true;
+					break;
+				}
+
+				if(reverse)
+				{
+					--slotIndex;
+				}
+				else
+				{
+					++slotIndex;
+				}
+			}
+		}
+
+		return successful;
+	}
 }
