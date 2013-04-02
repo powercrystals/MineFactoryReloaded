7b807ce60b3eab03ea5f3b86e188e83229ee5114
Emy <emy@fake.email>
2013-04-02 13:27:32 -0600

    dropStack now returns more null stacks
    The idea is that you can setInventorySlotContents(slotnumber,
    dropStack(inventory.getStackInSlot(slotnumber)));. Damn, that looks
    awkward.
    
    But seriously though, being able to simply set the new inventory
    contents to the value of dropStack(old contents) should be nifty.
    Hopefully this doesn't break Item Routers or whatever.

--- src/powercrystals/minefactoryreloaded/core/MFRInventoryUtil.java
+++ src/powercrystals/minefactoryreloaded/core/MFRInventoryUtil.java
@@ -154,8 +154,7 @@ public abstract class MFRInventoryUtil
 			if(pipe.getValue().acceptItems())
 			{
 				pipe.getValue().entityEntering(stack.copy(), pipe.getKey());
-				stack.stackSize = 0;
-				return stack;
+				return null;
 			}
 		}
 		// (2) Try to put stack in chests that are in valid directions
@@ -180,7 +179,7 @@ public abstract class MFRInventoryUtil
 			stack = manager.addItem(stack);
 			if(stack == null || stack.stackSize == 0)
 			{
-				return stack;
+				return null;
 			}
 		}
 		// (3) Having failed to put it in a chest or a pipe, throw it on the fucking ground. If airdropdirection is a valid direction.
@@ -189,7 +188,7 @@ public abstract class MFRInventoryUtil
 		if(Arrays.asList(ForgeDirection.VALID_DIRECTIONS).contains(airdropdirection) && !from.worldObj.isBlockSolidOnSide(from.xCoord, from.yCoord, from.zCoord, airdropdirection.getOpposite()))
 		{
 			dropStackOnGround(stack, bp, from.worldObj, airdropdirection);
-			stack.stackSize = 0;
+			return null;
 		}
 		// (4) Is the stack still here? :( Better give it back.
 		return stack;
