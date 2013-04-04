4adb7ac1db32997361a1cbb31f5be735229ffa47
Emy <emy@fake.email>
2013-04-04 11:41:33 -0600

    Dirty NPE fix revision attempt

--- src/powercrystals/minefactoryreloaded/tile/machine/TileEntityOilFabricator.java
+++ src/powercrystals/minefactoryreloaded/tile/machine/TileEntityOilFabricator.java
@@ -14,7 +14,7 @@ public class TileEntityOilFabricator extends TileEntityLiquidFabricator
 {
 	public TileEntityOilFabricator()
 	{
-		super((Integer)LiquidDictionary.getCanonicalLiquid("oil").itemID == null ? Block.waterStill.blockID : LiquidDictionary.getCanonicalLiquid("oil").itemID, 5880, 1);
+		super(LiquidDictionary.getCanonicalLiquid("oil") == null ? Block.waterStill.blockID : LiquidDictionary.getCanonicalLiquid("oil").itemID, 5880, 1);
 	}
 	
 	@Override
