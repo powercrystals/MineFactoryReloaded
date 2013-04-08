1ee0042ce518d54e4754593a04941826bd6b207e
powercrystals <powcrystals@gmail.com>
2013-04-08 15:05:18 -0400

    Fixes #58

--- src/powercrystals/minefactoryreloaded/net/IMFRProxy.java
+++ src/powercrystals/minefactoryreloaded/net/IMFRProxy.java
@@ -1,10 +1,13 @@
 package powercrystals.minefactoryreloaded.net;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.client.event.TextureStitchEvent;
 
 public interface IMFRProxy
 {
 	public void init();
 	
 	public void movePlayerToCoordinates(EntityPlayer e, double x, double y, double z);
+	
+	public void onPostTextureStitch(TextureStitchEvent.Post e);
 }
