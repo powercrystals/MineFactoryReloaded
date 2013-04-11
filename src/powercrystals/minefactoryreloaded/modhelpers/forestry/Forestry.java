073b0bd97fd244b6d3becb0b560baf49c3e936fa
powercrystals <powcrystals@gmail.com>
2013-04-11 00:01:30 -0400

    Fix Forestry compat so MFR gets shared liquids

--- src/powercrystals/minefactoryreloaded/modhelpers/forestry/Forestry.java
+++ src/powercrystals/minefactoryreloaded/modhelpers/forestry/Forestry.java
@@ -1,7 +1,15 @@
 package powercrystals.minefactoryreloaded.modhelpers.forestry;
 
+import java.lang.reflect.Field;
+
+import buildcraft.api.recipes.RefineryRecipe;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.block.Block;
+import net.minecraftforge.liquids.LiquidContainerData;
+import net.minecraftforge.liquids.LiquidContainerRegistry;
+import net.minecraftforge.liquids.LiquidDictionary;
+import net.minecraftforge.liquids.LiquidStack;
 import powercrystals.minefactoryreloaded.MineFactoryReloadedCore;
 import powercrystals.minefactoryreloaded.MFRRegistry;
 import powercrystals.minefactoryreloaded.api.HarvestType;
@@ -14,7 +22,9 @@ import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
 @Mod(modid = "MineFactoryReloaded|CompatForestry", name = "MFR Compat: Forestry", version = MineFactoryReloadedCore.version, dependencies = "after:MineFactoryReloaded;after:Forestry")
@@ -36,7 +46,7 @@ public class Forestry
 			{
 				MFRRegistry.registerFertilizer(new FertilizerStandard(fertilizer.itemID, 0));
 			}
-			Class forestryBlocks = Class.forName("forestry.core.config.ForestryBlock");
+			Class<?> forestryBlocks = Class.forName("forestry.core.config.ForestryBlock");
 			if(forestryBlocks != null)
 			{
 				MFRRegistry.registerHarvestable(new HarvestableStandard(((Block)forestryBlocks.getField("log1").get(null)).blockID, HarvestType.Tree));
@@ -56,6 +66,37 @@ public class Forestry
 			}
 		}
 		catch (Exception x)
+		{
+			x.printStackTrace();
+		}
+	}
+	
+	@PostInit
+	public static void postInit(FMLPostInitializationEvent e)
+	{
+		LiquidContainerRegistry.registerLiquid(new LiquidContainerData(LiquidDictionary.getOrCreateLiquid("milk", new LiquidStack(MineFactoryReloadedCore.milkStill,  LiquidContainerRegistry.BUCKET_VOLUME)), new ItemStack(Item.bucketMilk), new ItemStack(Item.bucketEmpty)));
+		LiquidContainerRegistry.registerLiquid(new LiquidContainerData(LiquidDictionary.getOrCreateLiquid("sludge", new LiquidStack(MineFactoryReloadedCore.sludgeStill,  LiquidContainerRegistry.BUCKET_VOLUME)), new ItemStack(MineFactoryReloadedCore.sludgeBucketItem), new ItemStack(Item.bucketEmpty)));
+		LiquidContainerRegistry.registerLiquid(new LiquidContainerData(LiquidDictionary.getOrCreateLiquid("sewage", new LiquidStack(MineFactoryReloadedCore.sewageStill,  LiquidContainerRegistry.BUCKET_VOLUME)), new ItemStack(MineFactoryReloadedCore.sewageBucketItem), new ItemStack(Item.bucketEmpty)));
+		LiquidContainerRegistry.registerLiquid(new LiquidContainerData(LiquidDictionary.getOrCreateLiquid("mobEssence", new LiquidStack(MineFactoryReloadedCore.essenceStill,  LiquidContainerRegistry.BUCKET_VOLUME)), new ItemStack(MineFactoryReloadedCore.mobEssenceBucketItem), new ItemStack(Item.bucketEmpty)));
+		LiquidContainerRegistry.registerLiquid(new LiquidContainerData(LiquidDictionary.getOrCreateLiquid("biofuel", new LiquidStack(MineFactoryReloadedCore.biofuelStill,  LiquidContainerRegistry.BUCKET_VOLUME)), new ItemStack(MineFactoryReloadedCore.bioFuelBucketItem), new ItemStack(Item.bucketEmpty)));
+		
+		try
+		{
+			Class<?> forestryItems = Class.forName("forestry.core.config.ForestryItem");
+			int forestryBiofuelId = ((Item)forestryItems.getField("liquidBiofuel").get(null)).itemID;
+			
+			for(RefineryRecipe rr : RefineryRecipe.getRecipes())
+			{
+				if(rr.result.itemID == forestryBiofuelId)
+				{
+					LiquidStack newResult = new LiquidStack(LiquidDictionary.getCanonicalLiquid("biofuel").itemID, rr.result.amount, LiquidDictionary.getCanonicalLiquid("biofuel").itemMeta);
+					Field result = RefineryRecipe.class.getField("result");
+					result.setAccessible(true);
+					result.set(rr, newResult);
+				}
+			}
+		}
+		catch(Exception x)
 		{
 			x.printStackTrace();
 		}
