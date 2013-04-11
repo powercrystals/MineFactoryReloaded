064ef1c59b88eeca5eaf1e909978266334ba132a
Emy <emy@fake.email>
2013-04-11 15:31:39 -0600

    Trying to keep up with Atum as it adds blocks

--- src/powercrystals/minefactoryreloaded/modhelpers/FertilizableSaplingReflection.java
+++ src/powercrystals/minefactoryreloaded/modhelpers/FertilizableSaplingReflection.java
@@ -5,25 +5,19 @@ import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.world.World;
+import powercrystals.minefactoryreloaded.MFRRegistry;
 import powercrystals.minefactoryreloaded.api.FertilizerType;
 import powercrystals.minefactoryreloaded.api.IFactoryFertilizable;
 
-public class FertilizableCropReflection implements IFactoryFertilizable
+public class FertilizableSaplingReflection implements IFactoryFertilizable
 {
 	private Method _fertilize;
 	private int _blockId;
-	private int _targetMeta;
 	
-	public FertilizableCropReflection(int blockId, Method fertilize)
-	{
-		this(blockId, fertilize, 7);
-	}
-	
-	public FertilizableCropReflection(int blockId, Method fertilize, int targetMeta)
+	public FertilizableSaplingReflection(int blockId, Method fertilize)
 	{
 		_blockId = blockId;
 		_fertilize = fertilize;
-		_targetMeta = targetMeta;
 	}
 	
 	@Override
@@ -35,7 +29,7 @@ public class FertilizableCropReflection implements IFactoryFertilizable
 	@Override
 	public boolean canFertilizeBlock(World world, int x, int y, int z, FertilizerType fertilizerType)
 	{
-		return world.getBlockMetadata(x, y, z) < _targetMeta && fertilizerType == FertilizerType.GrowPlant;
+		return fertilizerType == FertilizerType.GrowPlant;
 	}
 
 	@Override
@@ -49,6 +43,7 @@ public class FertilizableCropReflection implements IFactoryFertilizable
 		{
 			e.printStackTrace();
 		}
-		return world.getBlockMetadata(x, y, z) >= _targetMeta;
+		//return true if the target coords no longer contain a fertilizable block
+		return !MFRRegistry.getFertilizables().containsKey(world.getBlockId(x, y, z));
 	}
 }
--- src/powercrystals/minefactoryreloaded/modhelpers/atum/Atum.java
+++ src/powercrystals/minefactoryreloaded/modhelpers/atum/Atum.java
@@ -1,8 +1,12 @@
 package powercrystals.minefactoryreloaded.modhelpers.atum;
 
+import java.lang.reflect.Method;
+import java.util.Random;
+
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import powercrystals.minefactoryreloaded.MineFactoryReloadedCore;
 import powercrystals.minefactoryreloaded.MFRRegistry;
 import powercrystals.minefactoryreloaded.api.HarvestType;
@@ -10,6 +14,9 @@ import powercrystals.minefactoryreloaded.api.MobDrop;
 import powercrystals.minefactoryreloaded.farmables.grindables.GrindableStandard;
 import powercrystals.minefactoryreloaded.farmables.harvestables.HarvestableStandard;
 import powercrystals.minefactoryreloaded.farmables.harvestables.HarvestableTreeLeaves;
+import powercrystals.minefactoryreloaded.farmables.plantables.PlantableStandard;
+import powercrystals.minefactoryreloaded.modhelpers.FertilizableCropReflection;
+import powercrystals.minefactoryreloaded.modhelpers.FertilizableSaplingReflection;
 import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
@@ -38,31 +45,49 @@ public class Atum
 			String entityprefix = "rebelkeithy.mods.atum.entities.Entity";
 						
 			Class banditArcher = Class.forName(entityprefix + "BanditArcher");
+			Class banditWarlord = Class.forName(entityprefix + "BanditWarlord");
 			Class banditWarrior = Class.forName(entityprefix + "BanditWarrior");
 			Class dustySkeleton = Class.forName(entityprefix + "DustySkeleton");
+			Class desertWolf = Class.forName(entityprefix + "DesertWolf");
 			Class ghost = Class.forName(entityprefix + "Ghost");
 			Class mummy = Class.forName(entityprefix + "Mummy");
 			Class pharaoh = Class.forName(entityprefix + "Pharaoh");
 			Class stoneSoldier = Class.forName(entityprefix + "StoneSoldier");
 			
-			ItemStack scimitar = new ItemStack((Item)Atum.getField("itemScimitar").get(null));
+			
 			ItemStack clothScrap = new ItemStack((Item)Atum.getField("itemClothScrap").get(null));
 			ItemStack ectoplasm = new ItemStack((Item)Atum.getField("itemEctoplasm").get(null));
+			ItemStack pelt = new ItemStack((Item)Atum.getField("itemPelt").get(null));
+			ItemStack scimitar = new ItemStack((Item)Atum.getField("itemScimitar").get(null));
 			ItemStack stoneChunk = new ItemStack((Item)Atum.getField("itemStoneChunk").get(null));
 			
-			int atumLogID = ((Block)Atum.getField("atumLog").get(null)).blockID;
-			int atumLeavesID = ((Block)Atum.getField("atumLeaves").get(null)).blockID;
-			int papyrusID = ((Block)Atum.getField("atumPapyrus").get(null)).blockID;
-			int shrubID = ((Block)Atum.getField("atumShrub").get(null)).blockID;
-			int weedID = ((Block)Atum.getField("atumWeed").get(null)).blockID;
+			int flaxSeedsId = ((Item)Atum.getField("itemFlaxSeeds").get(null)).itemID;
 			
+			int atumLogId = ((Block)Atum.getField("atumLog").get(null)).blockID;
+			int atumLeavesId = ((Block)Atum.getField("atumLeaves").get(null)).blockID;
+			int atumSaplingId = ((Block)Atum.getField("atumLog").get(null)).blockID;
+			
+			int flaxId = ((Block)Atum.getField("atumFlax").get(null)).blockID;
+			int papyrusId = ((Block)Atum.getField("atumPapyrus").get(null)).blockID;
+			int shrubId = ((Block)Atum.getField("atumShrub").get(null)).blockID;
+			int weedId = ((Block)Atum.getField("atumWeed").get(null)).blockID;
+			
+			Method atumSaplingGrowTree = Class.forName("rebelkeithy.mods.atum.blocks.BlockAtumSapling").getMethod("growTree", World.class, int.class, int.class, int.class, Random.class);
+			Method atumFlaxFertilize = Class.forName("rebelkeithy.mods.atum.blocks.BlockFlax").getMethod("fertilize", World.class, int.class, int.class, int.class);
+			
+			MFRRegistry.registerSafariNetBlacklist(banditWarlord);
 			MFRRegistry.registerSafariNetBlacklist(pharaoh);
-			
+						
 			MFRRegistry.registerGrindable(new GrindableStandard(banditArcher, new ItemStack(Item.arrow, 2)));
 			MFRRegistry.registerGrindable(new GrindableStandard(banditWarrior, new MobDrop[]
 					{
 						new MobDrop(1, scimitar),
 						new MobDrop(9, null)
+					}));
+			MFRRegistry.registerGrindable(new GrindableStandard(desertWolf, new MobDrop[]
+					{
+						new MobDrop(1, pelt),
+						new MobDrop(1, new ItemStack(Item.bone))
 					}));
 			MFRRegistry.registerGrindable(new GrindableStandard(dustySkeleton, new ItemStack(Item.bone, 1)));
 			MFRRegistry.registerGrindable(new GrindableStandard(ghost, ectoplasm));
@@ -73,11 +98,19 @@ public class Atum
 					}));
 			MFRRegistry.registerGrindable(new GrindableStandard(stoneSoldier, stoneChunk));
 			
-			MFRRegistry.registerHarvestable(new HarvestableStandard(atumLogID, HarvestType.Tree));
-			MFRRegistry.registerHarvestable(new HarvestableTreeLeaves(atumLeavesID));
-			MFRRegistry.registerHarvestable(new HarvestableStandard(papyrusID, HarvestType.LeaveBottom));
-			MFRRegistry.registerHarvestable(new HarvestableStandard(shrubID, HarvestType.Normal));
-			MFRRegistry.registerHarvestable(new HarvestableStandard(weedID, HarvestType.Normal));
+			MFRRegistry.registerPlantable(new PlantableStandard(atumSaplingId, atumSaplingId));
+			MFRRegistry.registerPlantable(new PlantableStandard(flaxSeedsId, flaxId));
+			
+			MFRRegistry.registerFertilizable(new FertilizableCropReflection(flaxId, atumFlaxFertilize, 5));
+			MFRRegistry.registerFertilizable(new FertilizableSaplingReflection(atumSaplingId, atumSaplingGrowTree));
+			
+			MFRRegistry.registerHarvestable(new HarvestableStandard(atumLogId, HarvestType.Tree));
+			MFRRegistry.registerHarvestable(new HarvestableTreeLeaves(atumLeavesId));
+			MFRRegistry.registerHarvestable(new HarvestableStandard(flaxId, HarvestType.Normal));
+			MFRRegistry.registerHarvestable(new HarvestableStandard(papyrusId, HarvestType.LeaveBottom));
+			MFRRegistry.registerHarvestable(new HarvestableStandard(shrubId, HarvestType.Normal));
+			MFRRegistry.registerHarvestable(new HarvestableStandard(weedId, HarvestType.Normal));
+			
 		}
 		catch (Exception x)
 		{
