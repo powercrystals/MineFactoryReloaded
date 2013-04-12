7e10b7c1f5e879377cb0eecb784b2a187d83a1c8
powercrystals <powcrystals@gmail.com>
2013-04-11 22:44:25 -0400

    Update version

--- src/powercrystals/minefactoryreloaded/MineFactoryReloadedCore.java
+++ src/powercrystals/minefactoryreloaded/MineFactoryReloadedCore.java
@@ -119,7 +119,7 @@ public class MineFactoryReloadedCore extends BaseMod implements IUpdateableMod
 	
 	public static final String modId = "MineFactoryReloaded";
 	public static final String modNetworkChannel = "MFReloaded";
-	public static final String version = "1.5.1R2.4.0";
+	public static final String version = "1.5.1R2.5.0B1";
 	public static final String modName = "Minefactory Reloaded";
 	
 	public static final String guiFolder = "/powercrystals/minefactoryreloaded/textures/gui/";
