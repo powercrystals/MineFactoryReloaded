df4b1a17f7d74956bc72631d41b43b773530f5f6
GSValore <gsvalore@gmail.com>
2013-04-16 21:12:59 -0500

    Added support for ChocoCraft

---
+++ src/powercrystals/minefactoryreloaded/modhelpers/chococraft/Chococraft/Chococraft.java
@@ -0,0 +1,76 @@
+package powercrystals.minefactoryreloaded.modhelpers.chococraft.Chococraft;
+
+import cpw.mods.fml.common.FMLLog;
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+import net.minecraft.block.Block;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.item.Item;
+import net.minecraft.world.World;
+import powercrystals.minefactoryreloaded.MFRRegistry;
+import powercrystals.minefactoryreloaded.MineFactoryReloadedCore;
+import powercrystals.minefactoryreloaded.farmables.plantables.PlantableCropPlant;
+import powercrystals.minefactoryreloaded.modhelpers.FertilizableCropReflection;
+
+import java.lang.reflect.Method;
+
+@Mod(modid="MineFactoryReloaded|CompatChococraft", name = "MFR Compat: Chococraft",
+		version = MineFactoryReloadedCore.version,
+		dependencies = "required-after:MineFactoryReloaded;after:chococraft")
+@NetworkMod(clientSideRequired=false, serverSideRequired=false)
+public class Chococraft
+{
+	@Mod.Init
+	public void load(FMLInitializationEvent event)
+	{
+		if (!Loader.isModLoaded("chococraft"))
+		{
+			FMLLog.info("Chococraft is not available; MFR Chococraft Compat not loaded");
+			return;
+		}
+
+		try
+		{
+			Class<?> mod = Class.forName("chococraft.common.ModChocoCraft");
+
+			FMLLog.info("Registering Gysahls for Planter/Harvester/Fertilizer");
+			int blockId = ((Block)(mod.getField("gysahlStemBlock").get(null))).blockID;
+			int seedId = ((Item)(mod.getField("gysahlSeedsItem").get(null))).itemID;
+			Method fertilize = Class.forName("chococraft.common.items.BlockGysahlStem").getMethod("onBonemealUse", World.class, int.class, int.class, int.class);
+
+			MFRRegistry.registerPlantable(new PlantableCropPlant(seedId, blockId));
+			MFRRegistry.registerHarvestable(new HarvestableChococraft(blockId));
+			MFRRegistry.registerFertilizable(new FertilizableCropReflection(blockId, fertilize, 4));
+
+			// Chocobo List
+			FMLLog.info("Registering Chocobos for Grinder...");
+			final String[] chocobos = { "Black", "Blue", "Gold", "Green", "Pink", "Purple", "Red", "White", "Yellow" };
+			int legItem = ((Item)(mod.getField("chocoboLegRawItem").get(null))).itemID;
+			int featherItem = ((Item)(mod.getField("chocoboFeatherItem").get(null))).itemID;
+
+			for (String name : chocobos)
+			{
+				try
+				{
+					Class<?> chocoboClass = Class.forName(String.format("chococraft.common.entities.colours.EntityChocobo%s", name));
+					MFRRegistry.registerGrindable(new GrindableChocobo(chocoboClass, legItem, featherItem));
+				}
+				catch (ClassNotFoundException e)
+				{
+					FMLLog.warning(String.format("Entity class for the %s Chocobo could not be found", name));
+				}
+			}
+		}
+		catch (ClassNotFoundException e)
+		{
+			FMLLog.warning("Unable to load support for Chococraft");
+			e.printStackTrace();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/powercrystals/minefactoryreloaded/modhelpers/chococraft/Chococraft/HarvestableChococraft.java
@@ -0,0 +1,66 @@
+package powercrystals.minefactoryreloaded.modhelpers.chococraft.Chococraft;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import powercrystals.minefactoryreloaded.api.HarvestType;
+import powercrystals.minefactoryreloaded.api.IFactoryHarvestable;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class HarvestableChococraft implements IFactoryHarvestable
+{
+
+	private int blockId;
+
+	public HarvestableChococraft(int blockId)
+	{
+		this.blockId = blockId;
+	}
+
+	@Override
+	public int getPlantId()
+	{
+		return this.blockId;
+	}
+
+	@Override
+	public HarvestType getHarvestType()
+	{
+		return HarvestType.Normal;
+	}
+
+	@Override
+	public boolean breakBlock()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean canBeHarvested(World world, Map<String, Boolean> harvesterSettings, int x, int y, int z)
+	{
+		return world.getBlockMetadata(x, y, z) >= 4;
+	}
+
+	@Override
+	public List<ItemStack> getDrops(World world, Random rand, Map<String, Boolean> harvesterSettings, int x, int y, int z)
+	{
+		return Block.blocksList[blockId].getBlockDropped(world, x, y, z, world.getBlockMetadata(x, y, z), 0);
+	}
+
+	@Override
+	public void preHarvest(World world, int x, int y, int z)
+	{
+		if (world.getBlockMetadata(x, y, z) > 4)
+		{
+			world.setBlockMetadataWithNotify(x, y, z, 4, 2);
+		}
+	}
+
+	@Override
+	public void postHarvest(World world, int x, int y, int z)
+	{
+	}
+}
