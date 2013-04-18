8004c031b4f21ac153bf6c610023278fb3de329d
powercrystals <powcrystals@gmail.com>
2013-04-18 00:00:14 -0400

    Fix crash on opening a PRC while airborne???

--- src/powercrystals/minefactoryreloaded/gui/container/ContainerRedNetLogic.java
+++ src/powercrystals/minefactoryreloaded/gui/container/ContainerRedNetLogic.java
@@ -3,11 +3,18 @@ package powercrystals.minefactoryreloaded.gui.container;
 import powercrystals.minefactoryreloaded.tile.TileEntityRedNetLogic;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.item.ItemStack;
 
 public class ContainerRedNetLogic extends Container
 {
 	public ContainerRedNetLogic(TileEntityRedNetLogic logic)
 	{
+	}
+	
+	@Override
+	public void putStackInSlot(int par1, ItemStack par2ItemStack)
+	{
+		return;
 	}
 	
 	@Override
