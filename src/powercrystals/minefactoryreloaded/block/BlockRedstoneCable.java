17c7c67aade3ec5c399bd1cc18828d0842153c6f
powercrystals <powcrystals@gmail.com>
2013-04-17 20:32:54 -0400

    Fixed RedNet cable collision boxes (for movement)

--- src/powercrystals/minefactoryreloaded/block/BlockRedstoneCable.java
+++ src/powercrystals/minefactoryreloaded/block/BlockRedstoneCable.java
@@ -1,5 +1,7 @@
 package powercrystals.minefactoryreloaded.block;
 
+import java.util.List;
+
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import powercrystals.minefactoryreloaded.MineFactoryReloadedCore;
@@ -11,6 +13,7 @@ import powercrystals.minefactoryreloaded.tile.TileRedstoneCable;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -68,12 +71,12 @@ public class BlockRedstoneCable extends BlockContainer implements IRedNetNetwork
 		parts[1] = AxisAlignedBB.getBoundingBox(_wireStart, csd != RedNetConnectionType.None ? 0 : _wireStart, _wireStart, _wireEnd, csu != RedNetConnectionType.None ? 1 : _wireEnd, _wireEnd);
 		parts[2] = AxisAlignedBB.getBoundingBox(_wireStart, _wireStart, csn != RedNetConnectionType.None ? 0 : _wireStart, _wireEnd, _wireEnd, css != RedNetConnectionType.None ? 1 : _wireEnd);
 		
-		parts[3] = csw != RedNetConnectionType.PlateSingle ? null : AxisAlignedBB.getBoundingBox(0, _plateStart, _plateStart, _plateDepth, _plateEnd, _plateEnd);
-		parts[4] = cse != RedNetConnectionType.PlateSingle ? null : AxisAlignedBB.getBoundingBox(1.0F - _plateDepth, _plateStart, _plateStart, 1.0F, _plateEnd, _plateEnd);
-		parts[5] = csd != RedNetConnectionType.PlateSingle ? null : AxisAlignedBB.getBoundingBox(_plateStart, 0 , _plateStart, _plateEnd, _plateDepth, _plateEnd);
-		parts[6] = csu != RedNetConnectionType.PlateSingle ? null : AxisAlignedBB.getBoundingBox(_plateStart, 1.0F - _plateDepth, _plateStart, _plateEnd, 1.0F, _plateEnd);
-		parts[7] = csn != RedNetConnectionType.PlateSingle ? null : AxisAlignedBB.getBoundingBox(_plateStart, _plateStart, 0, _plateEnd, _plateDepth, _plateEnd);
-		parts[8] = css != RedNetConnectionType.PlateSingle ? null : AxisAlignedBB.getBoundingBox(_plateStart, _plateStart, 1.0F - _plateDepth, _plateEnd, _plateEnd, 1.0F);
+		parts[3] = csw != RedNetConnectionType.PlateSingle && csw != RedNetConnectionType.PlateAll ? null : AxisAlignedBB.getBoundingBox(0, _plateStart, _plateStart, _plateDepth, _plateEnd, _plateEnd);
+		parts[4] = cse != RedNetConnectionType.PlateSingle && cse != RedNetConnectionType.PlateAll ? null : AxisAlignedBB.getBoundingBox(1.0F - _plateDepth, _plateStart, _plateStart, 1.0F, _plateEnd, _plateEnd);
+		parts[5] = csd != RedNetConnectionType.PlateSingle && csd != RedNetConnectionType.PlateAll ? null : AxisAlignedBB.getBoundingBox(_plateStart, 0 , _plateStart, _plateEnd, _plateDepth, _plateEnd);
+		parts[6] = csu != RedNetConnectionType.PlateSingle && csu != RedNetConnectionType.PlateAll ? null : AxisAlignedBB.getBoundingBox(_plateStart, 1.0F - _plateDepth, _plateStart, _plateEnd, 1.0F, _plateEnd);
+		parts[7] = csn != RedNetConnectionType.PlateSingle && csn != RedNetConnectionType.PlateAll ? null : AxisAlignedBB.getBoundingBox(_plateStart, _plateStart, 0, _plateEnd, _plateDepth, _plateEnd);
+		parts[8] = css != RedNetConnectionType.PlateSingle && css != RedNetConnectionType.PlateAll ? null : AxisAlignedBB.getBoundingBox(_plateStart, _plateStart, 1.0F - _plateDepth, _plateEnd, _plateEnd, 1.0F);
 		
 		parts[9]  = csw != RedNetConnectionType.PlateSingle && csw != RedNetConnectionType.CableSingle ? null : AxisAlignedBB.getBoundingBox(_bandDepthStart, _bandWidthStart, _bandWidthStart, _bandDepthEnd, _bandWidthEnd, _bandWidthEnd);
 		parts[10] = cse != RedNetConnectionType.PlateSingle && cse != RedNetConnectionType.CableSingle ? null : AxisAlignedBB.getBoundingBox(1.0F - _bandDepthEnd, _bandWidthStart, _bandWidthStart, 1.0F - _bandDepthStart, _bandDepthEnd, _bandWidthEnd);
@@ -160,20 +163,67 @@ public class BlockRedstoneCable extends BlockContainer implements IRedNetNetwork
 	@Override
 	public void setBlockBoundsBasedOnState(IBlockAccess world, int x, int y, int z)
 	{
-		super.setBlockBoundsBasedOnState(world, x, y, z);
-		/*
-		TileEntity te = world.getBlockTileEntity(x, y, z);
-		if(te != null && te instanceof TileRedstoneCable)
+		float xMin = 1;
+		float yMin = 1;
+		float zMin = 1;
+		float xMax = 0;
+		float yMax = 0;
+		float zMax = 0;
+		
+		TileEntity cable = world.getBlockTileEntity(x, y, z);
+		if(cable instanceof TileRedstoneCable)
 		{
-			TileRedstoneCable cable = (TileRedstoneCable)te;
-			float xMin = cable.getConnectionState(ForgeDirection.WEST) != ConnectionState.None ? 0 : 0.375F;
-			float xMax = cable.getConnectionState(ForgeDirection.EAST) != ConnectionState.None ? 1 : 0.625F;
-			float yMin = cable.getConnectionState(ForgeDirection.DOWN) != ConnectionState.None ? 0 : 0.375F;
-			float yMax = cable.getConnectionState(ForgeDirection.UP) != ConnectionState.None ? 1 : 0.625F;
-			float zMin = cable.getConnectionState(ForgeDirection.NORTH) != ConnectionState.None ? 0 : 0.375F;
-			float zMax = cable.getConnectionState(ForgeDirection.SOUTH) != ConnectionState.None ? 1 : 0.625F;
+			for(AxisAlignedBB aabb : getParts((TileRedstoneCable)cable))
+			{
+				if(aabb == null)
+				{
+					continue;
+				}
+				
+				xMin = Math.min(xMin, (float)aabb.minX);
+				yMin = Math.min(yMin, (float)aabb.minY);
+				zMin = Math.min(zMin, (float)aabb.minZ);
+				xMax = Math.max(xMax, (float)aabb.maxX);
+				yMax = Math.max(yMax, (float)aabb.maxY);
+				zMax = Math.max(zMax, (float)aabb.maxZ);
+			}
 			setBlockBounds(xMin, yMin, zMin, xMax, yMax, zMax);
-		}*/
+		}
+		else
+		{
+			super.setBlockBoundsBasedOnState(world, x, y, z);
+		}
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	public void addCollisionBoxesToList(World world, int x, int y, int z, AxisAlignedBB collisionTest, List collisionBoxList, Entity entity)
+	{
+		TileEntity cable = world.getBlockTileEntity(x, y, z);
+		if(cable instanceof TileRedstoneCable)
+		{
+			for(AxisAlignedBB aabb : getParts((TileRedstoneCable)cable))
+			{
+				if(aabb == null)
+				{
+					continue;
+				}
+				aabb.minX += x;
+				aabb.maxX += x;
+				aabb.minY += y;
+				aabb.maxY += y;
+				aabb.minZ += z;
+				aabb.maxZ += z;
+				if(collisionTest.intersectsWith(aabb))
+				{
+					collisionBoxList.add(aabb);
+				}
+			}
+		}
+		else
+		{
+			super.addCollisionBoxesToList(world, x, y, z, collisionTest, collisionBoxList, entity);
+		}
 	}
 	
 	@Override
