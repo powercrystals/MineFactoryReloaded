3de204e0f1c02902a9e8b44028e1ed42249cec27
Emy <emy@fake.email>
2013-04-17 22:26:48 -0600

    Changed Collector to TEFactoryInventory

--- src/powercrystals/minefactoryreloaded/tile/machine/TileEntityCollector.java
+++ src/powercrystals/minefactoryreloaded/tile/machine/TileEntityCollector.java
@@ -3,9 +3,9 @@ package powercrystals.minefactoryreloaded.tile.machine;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemStack;
 import powercrystals.minefactoryreloaded.core.MFRInventoryUtil;
-import powercrystals.minefactoryreloaded.tile.base.TileEntityFactory;
+import powercrystals.minefactoryreloaded.tile.base.TileEntityFactoryInventory;
 
-public class TileEntityCollector extends TileEntityFactory
+public class TileEntityCollector extends TileEntityFactoryInventory
 {
 	public void addToChests(EntityItem i)
 	{
@@ -22,5 +22,15 @@ public class TileEntityCollector extends TileEntityFactory
 			return;
 		}
 		i.setEntityItemStack(s);
+	}
+	
+	public int getSizeInventory()
+	{
+		return 0;
+	}
+	
+	public String getInvName()
+	{
+		return "Item Collector";
 	}
 }
