9587e0d87e585bb002cb2af47a88fe1671f979a7
powercrystals <powcrystals@gmail.com>
2013-04-18 20:34:37 -0400

    Added pulse formers

---
+++ src/powercrystals/minefactoryreloaded/circuits/timing/OneShot.java
@@ -0,0 +1,67 @@
+package powercrystals.minefactoryreloaded.circuits.timing;
+
+import net.minecraft.nbt.NBTTagCompound;
+import powercrystals.minefactoryreloaded.api.rednet.IRedNetLogicCircuit;
+
+public class OneShot implements IRedNetLogicCircuit
+{
+	private boolean _lastState;
+	
+	@Override
+	public int getInputCount()
+	{
+		return 1;
+	}
+
+	@Override
+	public int getOutputCount()
+	{
+		return 1;
+	}
+
+	@Override
+	public int[] recalculateOutputValues(long worldTime, int[] inputValues)
+	{
+		int output;
+		if(inputValues[0] > 0 && !_lastState)
+		{
+			output = 15;
+		}
+		else
+		{
+			output = 0;
+		}
+		_lastState = inputValues[0] > 0;
+		return new int[] { output };
+	}
+
+	@Override
+	public String getUnlocalizedName()
+	{
+		return "circuit.mfr.oneshot";
+	}
+
+	@Override
+	public String getInputPinLabel(int pin)
+	{
+		return "I";
+	}
+
+	@Override
+	public String getOutputPinLabel(int pin)
+	{
+		return "O";
+	}
+
+	@Override
+	public void readFromNBT(NBTTagCompound tag)
+	{
+		_lastState = tag.getBoolean("lastState");
+	}
+
+	@Override
+	public void writeToNBT(NBTTagCompound tag)
+	{
+		tag.setBoolean("lastState", _lastState);
+	}
+}
---
+++ src/powercrystals/minefactoryreloaded/circuits/timing/PulseLengthener.java
@@ -0,0 +1,77 @@
+package powercrystals.minefactoryreloaded.circuits.timing;
+
+import net.minecraft.nbt.NBTTagCompound;
+import powercrystals.minefactoryreloaded.api.rednet.IRedNetLogicCircuit;
+
+public class PulseLengthener implements IRedNetLogicCircuit
+{
+	private int _pulseLength;
+	private int _pulseCountdown;
+	
+	@Override
+	public int getInputCount()
+	{
+		return 2;
+	}
+
+	@Override
+	public int getOutputCount()
+	{
+		return 1;
+	}
+
+	@Override
+	public int[] recalculateOutputValues(long worldTime, int[] inputValues)
+	{
+		_pulseLength = inputValues[1];
+		int output;
+		
+		if(_pulseCountdown <= 0 && inputValues[0] > 0)
+		{
+			_pulseCountdown = _pulseLength;
+			output = 15;
+		}
+		else if(_pulseCountdown > 0)
+		{
+			_pulseCountdown--;
+			output = 15;
+		}
+		else
+		{
+			output = 0;
+		}
+		return new int[] { output };
+	}
+
+	@Override
+	public String getUnlocalizedName()
+	{
+		return "circuit.mfr.pulselengthener";
+	}
+
+	@Override
+	public String getInputPinLabel(int pin)
+	{
+		return pin == 0 ? "I" : "L";
+	}
+
+	@Override
+	public String getOutputPinLabel(int pin)
+	{
+		return "O";
+	}
+
+	@Override
+	public void readFromNBT(NBTTagCompound tag)
+	{
+		_pulseLength = tag.getInteger("pulseLength");
+		_pulseCountdown = tag.getInteger("pulseCountdown");
+	}
+
+	@Override
+	public void writeToNBT(NBTTagCompound tag)
+	{
+		tag.setInteger("pulseLength", _pulseLength);
+		tag.setInteger("pulseCountdown", _pulseCountdown);
+	}
+}
