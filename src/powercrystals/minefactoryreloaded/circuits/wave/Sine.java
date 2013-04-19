1183a36cda6e86aa84e2eb57a44bf1129fb2e90b
powercrystals <powcrystals@gmail.com>
2013-04-18 21:01:23 -0400

    Added Sine Wave Generator

---
+++ src/powercrystals/minefactoryreloaded/circuits/wave/Sine.java
@@ -0,0 +1,44 @@
+package powercrystals.minefactoryreloaded.circuits.wave;
+
+import powercrystals.minefactoryreloaded.api.rednet.IRedNetLogicCircuit;
+import powercrystals.minefactoryreloaded.circuits.base.StatelessCircuit;
+
+public class Sine extends StatelessCircuit implements IRedNetLogicCircuit
+{
+	@Override
+	public int getInputCount()
+	{
+		return 0;
+	}
+
+	@Override
+	public int getOutputCount()
+	{
+		return 1;
+	}
+
+	@Override
+	public int[] recalculateOutputValues(long worldTime, int[] inputValues)
+	{
+		int value = (int)(7.5 * (Math.sin(Math.PI / 8.0 * (worldTime % 16)) + 1));
+		return new int[] { value };
+	}
+
+	@Override
+	public String getUnlocalizedName()
+	{
+		return "circuit.mfr.wavegenerator.sine";
+	}
+
+	@Override
+	public String getInputPinLabel(int pin)
+	{
+		return "";
+	}
+
+	@Override
+	public String getOutputPinLabel(int pin)
+	{
+		return "Q";
+	}
+}
