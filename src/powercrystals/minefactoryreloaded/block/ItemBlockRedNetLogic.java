c48da569a4c6c572424187ff0929284aa3f01900
powercrystals <powcrystals@gmail.com>
2013-04-20 19:18:04 -0400

    Updated PRC manual

--- src/powercrystals/minefactoryreloaded/block/ItemBlockRedNetLogic.java
+++ src/powercrystals/minefactoryreloaded/block/ItemBlockRedNetLogic.java
@@ -52,27 +52,55 @@ public class ItemBlockRedNetLogic extends ItemBlock
 				"circuit type. Use the Next and Prev buttons to change which circuit is being edited."));
 		pages.appendTag(new NBTTagString("14", "GUI: Pin Mapping\n\nPRC pins are connected in two parts, buffer and pin number. Buffer selects I/O and face, " +
 				"variables, constants, or null, as applicable. Pin chooses which I/O color, or which variable/constant."));
-		pages.appendTag(new NBTTagString("15", "Examples\n\nThe next sections cover some basic PRC scenarios. Face your PRC West with your sledgehammer " +
-				"before beginning. Ensure you have levers, lamps, redstone dust, and RedNet cabling."));
-		pages.appendTag(new NBTTagString("16", "Example 1: And\n\nConnect RedNet cabling to the North face of your PRC, and place two levers and a lamp on " +
+		pages.appendTag(new NBTTagString("15", "GUI: Pin Mapping\n\nLeft click advances by one, and right click reverses by one. Middle click " +
+				"advances by 16 when using variables or constants (one for I/O)."));
+		pages.appendTag(new NBTTagString("16", "RedNet Meter\n\nMuch like it can be used to read out values of RedNet cables, the RedNet Meter can " +
+				"be used to read out the state of the PRC's variables."));
+		pages.appendTag(new NBTTagString("17", "Memory Cards\n\nIf you need to back up your PRC or copy it to another PRC, use a memory card. Using " +
+				"a blank card on a PRC will copy its program, and using a programmed card will write its program."));
+		pages.appendTag(new NBTTagString("18", "Examples\n\nThe next sections cover some basic PRC scenarios. Face your PRC West with your sledgehammer " +
+				"before beginning. These examples assume access to some supplies (next page)."));
+		pages.appendTag(new NBTTagString("19", "Examples\n\nYou will need:\n* A few dozen cables\n* Several levers\n* Several buttons\n* Several lamps\n" +
+				"* A daylight sensor\n* A piston\n* A note block\n* A sledgehammer"));
+		pages.appendTag(new NBTTagString("20", "Example 1: AND gate\n\nConnect RedNet cabling to the North face of your PRC, and place two levers and a lamp on " +
 				"the cable. Set the levers to Orange and Magenta with your sledgehammer."));
-		pages.appendTag(new NBTTagString("17", "Example 1: And\n\nOpen your PRC. Set circuit one to And (2 Input). Set the input I/Os to I/O N, Orange and " +
+		pages.appendTag(new NBTTagString("21", "Ex 1: AND gate\n\nOpen your PRC. Set circuit one to And (2 Input). Set the input I/Os to I/O N, Orange and " +
 				"Magenta, and the output to I/O N, White."));
-		pages.appendTag(new NBTTagString("18", "Example 1: And\n\nManipulate the levers. Watch the lamp. It will light when both levers are on " +
+		pages.appendTag(new NBTTagString("22", "Ex 1: AND gate\n\nManipulate the levers. Watch the lamp. It will light when both levers are on " +
 				"and deactivate when they are turned off."));
-		pages.appendTag(new NBTTagString("19", "Example 2: Timer\n\nConnect RedNet cabling to the South face of your PRC, and attach a lamp to it. " +
-				"Open your PRC and set circuit 2 to Wave (Square). Set the output to I/O S, White."));
-		pages.appendTag(new NBTTagString("20", "Example 2: Timer\n\nThe lamp will blink. You can use the square wave generator for any device " +
+		pages.appendTag(new NBTTagString("23", "Ex 2: Timer\n\nConnect RedNet cabling to the South face of your PRC, and attach a lamp to it. " +
+				"Open your PRC and set circuit 2 to Wave (Square). Set the input to CNST 40. Set the output to I/O S, White."));
+		pages.appendTag(new NBTTagString("24", "Ex 2: Timer\n\nThe lamp will blink about once per second. You can use the square wave generator for any device " +
 				"that requires an alternating redstone signal."));
-		pages.appendTag(new NBTTagString("21", "Example 3: Variables\n\nConnect RedNet cabling to the East face of your PRC, and run a line of " +
-				"redstone dust away from the cable."));
-		pages.appendTag(new NBTTagString("22", "Example 3: Variables\n\nOpen your PRC, and set circuit 3 to Wave (Triangle) and its output to " +
-				"variable 0 (under the VARS buffer)."));
-		pages.appendTag(new NBTTagString("23", "Example 3: Variables\n\nSet circuit 4 to Passthrough and its input to variable 0 and output to the " + 
-				" I/O E White. Watch the dust rise and fall in power."));
-		pages.appendTag(new NBTTagString("24", "Summary\n\nNote that the examples all function independently, as the PRC has multiple circuits and " +
+		pages.appendTag(new NBTTagString("25", "Ex 2: Timer\n\nThe PRC runs at a clock rate of 20 cycles per second. Adjust the period (Pd) of the " +
+				"timer to make it faster or slower. Values too fast (<5 t) may not work correctly with all devices."));
+		pages.appendTag(new NBTTagString("26", "Ex 3: Door Toggle\n\nConnect cabling to the East face of your PRC. Attach a button to White and a piston to Orange. " +
+				"If you have extra parts, you may want to build a real door."));
+		pages.appendTag(new NBTTagString("27", "Ex 3: Door Toggle\n\nOpen your PRC and set circuit 3 to a T-FlipFlop. Connect I/O E, White to both inputs, " +
+				"and I/O E Orange to Q. Press the button."));
+		pages.appendTag(new NBTTagString("28", "Ex 3: Door Toggle\n\nNote the piston extends and retracts on each button press. The T-FlipFlop samples the input " +
+				"value on a rising edge of CLK - if you don't care, connect both to the same input as here."));
+		pages.appendTag(new NBTTagString("29", "Ex 4: Daylight Sensor/Variables\n\nConnect RedNet cabling to the top of your PRC. Attach a daylight sensor to White, " +
+				"and a note block to Orange. Set a pleasing tone on the note block."));
+		pages.appendTag(new NBTTagString("30", "Ex 4: Daylight Sensor/Variables\n\nOpen the PRC. Set circuit 4 to Equals, with I/O U White and CNST 3 as inputs, and the " +
+				"output to VARS 0. Set circuit 5 to a One-Shot Pulse, with input VARS 0 and output I/O U Orange."));
+		pages.appendTag(new NBTTagString("31", "Ex 4: Daylight Sensor/Variables\n\nWhen the sun rises and sets, the daylight sensor's output level will reach 3. As it " +
+				"arrives at that value, the note block will be pulsed. Use it as an alarm clock!"));
+		pages.appendTag(new NBTTagString("32", "Ex 5: Daylight Clock\n\nConstruct an \"8\" out of redstone lamps. It should look like this:\n" +
+				" XX \nX  X\nX  X\n XX \nX  X\nX  X\n XX \nEnsure no blocks connect the lamps."));
+		pages.appendTag(new NBTTagString("33", "Ex 5: Daylight Clock\n\nStarting with the top section and proceeding clockwise, connect them as follows: " +
+				" cyan, purple, blue, brown, green, red, black. Red should be the top-left pair and black the center pair."));
+		pages.appendTag(new NBTTagString("34", "Ex 5: Daylight Clock\n\nConnect those wires to the top face of the PRC (along with the daylight sensor). " +
+				"Open the PRC, and set circuit 6 to Seven-Segment Encoder."));
+		pages.appendTag(new NBTTagString("35", "Ex 5: Daylight Clock\n\nConnect I to I/O U White, and A through G to cyan through black in order. If you " +
+				"have wired everything correctly, the clock will display the current daylight value."));
+		pages.appendTag(new NBTTagString("36", "Ex 5: Daylight Clock\n\nNote that as the daylight sensor has outputs 0-15, these are reprsented as hex characters " +
+				"as 0-9 and A-F."));
+		pages.appendTag(new NBTTagString("37", "Additional Suggestions\n\n* Try the various wave generators connected to redstone dust\n* Try using the counter - " +
+				"PRE is the rollover target.\n* Try using weighted pressure plates combined with greater/less than."));
+		pages.appendTag(new NBTTagString("38", "Summary\n\nNote that the examples all function independently, as the PRC has multiple circuits and " +
 				"can process many tasks at once. Amazing things can be accomplished using the PRC."));
-		pages.appendTag(new NBTTagString("25", "We hope you enjoy your new Programmable RedNet Controller!"));
+		pages.appendTag(new NBTTagString("39", "We hope you enjoy your new Programmable RedNet Controller!"));
 		
 		nbt.setTag("pages", pages);
 		manual = new ItemStack(Item.writtenBook);
