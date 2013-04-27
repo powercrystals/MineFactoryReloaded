6ef4ed4e60ae32880bc438f727ad95e44ed63c3a
Emy <emy@fake.email>
2013-04-26 18:50:59 -0600

    Added Circuit: Decompose Int -> Decimal

---
+++ src/powercrystals/minefactoryreloaded/circuits/analog/DecomposeIntToDecimal.java
@@ -0,0 +1,65 @@
+package powercrystals.minefactoryreloaded.circuits.analog;
+
+
+import powercrystals.minefactoryreloaded.api.rednet.IRedNetLogicCircuit;
+import powercrystals.minefactoryreloaded.circuits.base.StatelessCircuit;
+
+public class DecomposeIntToDecimal extends StatelessCircuit implements IRedNetLogicCircuit
+{
+	@Override
+	public int getInputCount()
+	{
+		return 1;
+	}
+
+	@Override
+	public int getOutputCount()
+	{
+		return 11;
+	}
+
+	@Override
+	public int[] recalculateOutputValues(long worldTime, int[] inputValues)
+	{
+		int[] returnArray = new int[11];
+		//add a sign bit to the first 
+		if(inputValues[0] >= 0)
+		{
+			returnArray[0] = 0;
+		}
+		else
+		{
+			returnArray[0] = 15;
+		}
+		char[] decimalChars = Integer.toString(Math.abs(inputValues[0])).toCharArray();
+		// put the decimal digits in the return array, little endian.
+		for(int i = decimalChars.length - 1 ; i >= 0 ; i--)
+		{
+			returnArray[decimalChars.length - i] = Character.digit(decimalChars[i], 10);
+		}
+		// fill the remainder of the array with zeroes.
+		for(int i = decimalChars.length + 1 ; i < returnArray.length ; i++)
+		{
+			returnArray[i] = 0;
+		}
+		return returnArray;
+	}
+
+	@Override
+	public String getUnlocalizedName()
+	{
+		return "circuit.mfr.decompose.decimal";
+	}
+
+	@Override
+	public String getInputPinLabel(int pin)
+	{
+		return "I";
+	}
+
+	@Override
+	public String getOutputPinLabel(int pin)
+	{
+		return pin == 0 ? "SN" : "^" + (pin - 1);
+	}
+}
--- src/powercrystals/minefactoryreloaded/modhelpers/vanilla/Vanilla.java
+++ src/powercrystals/minefactoryreloaded/modhelpers/vanilla/Vanilla.java
@@ -263,6 +263,7 @@ public class Vanilla
 		MFRRegistry.registerRedNetLogicCircuit(new And3());
 		MFRRegistry.registerRedNetLogicCircuit(new And4());
 		MFRRegistry.registerRedNetLogicCircuit(new Counter());
+		MFRRegistry.registerRedNetLogicCircuit(new DecomposeIntToDecimal());
 		MFRRegistry.registerRedNetLogicCircuit(new DeMux16Analog());
 		MFRRegistry.registerRedNetLogicCircuit(new DeMux4());
 		MFRRegistry.registerRedNetLogicCircuit(new Equal());
