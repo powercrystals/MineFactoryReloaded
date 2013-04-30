50adffbe4e29e286dc85a6f774ae984257e2a5c9
powercrystals <powcrystals@gmail.com>
2013-04-29 20:32:07 -0400

    Promoting to 2.5.2

--- src/powercrystals/minefactoryreloaded/MineFactoryReloadedCore.java
+++ src/powercrystals/minefactoryreloaded/MineFactoryReloadedCore.java
@@ -131,7 +131,7 @@ public class MineFactoryReloadedCore extends BaseMod implements IUpdateableMod
 	
 	public static final String modId = "MineFactoryReloaded";
 	public static final String modNetworkChannel = "MFReloaded";
-	public static final String version = "1.5.1R2.5.2B2";
+	public static final String version = "1.5.1R2.5.2";
 	public static final String modName = "Minefactory Reloaded";
 	
 	public static final String guiFolder = "/powercrystals/minefactoryreloaded/textures/gui/";
