8192dc656aded08848a411352ebac077e6094313
powercrystals <powcrystals@gmail.com>
2013-05-07 20:08:07 -0400

    Laser drill beam is properly lit

--- src/powercrystals/minefactoryreloaded/render/tileentity/LaserDrillRenderer.java
+++ src/powercrystals/minefactoryreloaded/render/tileentity/LaserDrillRenderer.java
@@ -2,6 +2,8 @@ package powercrystals.minefactoryreloaded.render.tileentity;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
@@ -21,15 +23,16 @@ public class LaserDrillRenderer extends TileEntitySpecialRenderer
 			this.bindTextureByName("/misc/beam.png");
 			GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, 10497.0F);
 			GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, 10497.0F);
-			//GL11.glDisable(GL11.GL_LIGHTING);
-			GL11.glEnable(GL11.GL_LIGHTING);
+			GL11.glPushAttrib(GL11.GL_LIGHTING_BIT);
+			RenderHelper.disableStandardItemLighting();
+			OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240.0F, 240.0F);
 			GL11.glDisable(GL11.GL_CULL_FACE);
 			GL11.glDisable(GL11.GL_BLEND);
 			GL11.glDepthMask(true);
 			GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE);
-			float ticks = (float) laserDrill.getWorldObj().getTotalWorldTime() + partialTicks;
-			float f3 = -ticks * 0.2F - (float) MathHelper.floor_float(-ticks * 0.1F);
-			double d3 = (double) ticks * 0.025D * (1.0D - 2.5D);
+			float ticks = (float)laserDrill.getWorldObj().getTotalWorldTime() + partialTicks;
+			float f3 = -ticks * 0.2F - (float)MathHelper.floor_float(-ticks * 0.1F);
+			double d3 = (double)ticks * 0.025D * (1.0D - 2.5D);
 			tessellator.startDrawingQuads();
 			tessellator.setColorRGBA(255, 255, 255, 32);
 			double d4 = 0.2D;
@@ -45,8 +48,8 @@ public class LaserDrillRenderer extends TileEntitySpecialRenderer
 			double height = (double)laserDrill.getBeamHeight();
 			double uStart = 0.0D;
 			double uEnd = 1.0D;
-			double vStart = (double) (-1.0F + f3);
-			double vEnd = (double) (256.0F) * (0.5D / d4) + vStart;
+			double vStart = (double)(-1.0F + f3);
+			double vEnd = (double)(256.0F) * (0.5D / d4) + vStart;
 			tessellator.addVertexWithUV(x + d5, yStart + height, z + d6, uEnd, vEnd);
 			tessellator.addVertexWithUV(x + d5, yStart, z + d6, uEnd, vStart);
 			tessellator.addVertexWithUV(x + d7, yStart, z + d8, uStart, vStart);
@@ -77,8 +80,8 @@ public class LaserDrillRenderer extends TileEntitySpecialRenderer
 			double d23 = 0.8D;
 			double d24 = 0.8D;
 			double d25 = 0.8D;
-			double d29 = (double) (-1.0F + f3);
-			double d30 = (double) (256.0F) + d29;
+			double d29 = (double)(-1.0F + f3);
+			double d30 = (double)(256.0F) + d29;
 			tessellator.addVertexWithUV(x + d18, yStart + height, z + d19, uEnd, d30);
 			tessellator.addVertexWithUV(x + d18, yStart, z + d19, uEnd, d29);
 			tessellator.addVertexWithUV(x + d20, yStart, z + d21, uStart, d29);
@@ -99,12 +102,13 @@ public class LaserDrillRenderer extends TileEntitySpecialRenderer
 			GL11.glEnable(GL11.GL_LIGHTING);
 			GL11.glEnable(GL11.GL_TEXTURE_2D);
 			GL11.glDepthMask(true);
+			GL11.glPopAttrib();
 		}
 	}
 
 	@Override
 	public void renderTileEntityAt(TileEntity tileEntity, double x, double y, double z, float partialTicks)
 	{
-		this.renderLaserDrillAt((TileEntityLaserDrill) tileEntity, x, y, z, partialTicks);
+		this.renderLaserDrillAt((TileEntityLaserDrill)tileEntity, x, y, z, partialTicks);
 	}
 }
