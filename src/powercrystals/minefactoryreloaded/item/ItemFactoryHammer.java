29b078490d7466958b5df73e5be9b3ad6b1b3250
powercrystals <powcrystals@gmail.com>
2013-05-11 18:46:21 -0400

    Hammer passes sneak-click to block; needs testing

--- src/powercrystals/minefactoryreloaded/item/ItemFactoryHammer.java
+++ src/powercrystals/minefactoryreloaded/item/ItemFactoryHammer.java
@@ -1,5 +1,6 @@
 package powercrystals.minefactoryreloaded.item;
 
+import net.minecraft.world.World;
 import powercrystals.minefactoryreloaded.api.IToolHammer;
 
 public class ItemFactoryHammer extends ItemFactory implements IToolHammer
@@ -7,5 +8,11 @@ public class ItemFactoryHammer extends ItemFactory implements IToolHammer
 	public ItemFactoryHammer(int i)
 	{
 		super(i);
+	}
+	
+	@Override
+	public boolean shouldPassSneakingClickToBlock(World world, int x, int y, int z)
+	{
+		return true;
 	}
 }
