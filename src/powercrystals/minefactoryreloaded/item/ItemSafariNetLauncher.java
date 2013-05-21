2da4edca82834bd0f5b3b054e940530d0e15e2f0
powercrystals <powcrystals@gmail.com>
2013-05-21 18:29:56 -0400

    Net launcher has separate capture/release modes

--- src/powercrystals/minefactoryreloaded/item/ItemSafariNetLauncher.java
+++ src/powercrystals/minefactoryreloaded/item/ItemSafariNetLauncher.java
@@ -1,5 +1,9 @@
 package powercrystals.minefactoryreloaded.item;
 
+import java.util.List;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
@@ -14,25 +18,59 @@ public class ItemSafariNetLauncher extends ItemFactory
 		setCreativeTab(MFRCreativeTab.tab);
 	}
 	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, EntityPlayer player, List infoList, boolean advancedTooltips)
+	{
+		super.addInformation(stack, player, infoList, advancedTooltips);
+		infoList.add("Sneak-click to change mode");
+	}
+	
 	@Override
 	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player)
 	{
-		world.playSoundAtEntity(player, "random.bow", 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
+		if(player.isSneaking())
+		{
+			stack.setItemDamage(stack.getItemDamage() == 0 ? 1 : 0);
+			if(world.isRemote)
+			{
+				if(isCaptureMode(stack))
+				{
+					player.sendChatToPlayer("Changed to capture mode");
+				}
+				else
+				{
+					player.sendChatToPlayer("Changed to release mode");
+				}
+			}
+			return stack;
+		}
 		
 		for(int i = 0; i < player.inventory.getSizeInventory(); i++)
 		{
 			ItemStack ammo = player.inventory.getStackInSlot(i);
 			if(ammo != null && ammo.getItem() instanceof ItemSafariNet)
 			{
-				if(!world.isRemote)
+				if((ItemSafariNet.isEmpty(ammo) && isCaptureMode(stack)) || (!ItemSafariNet.isEmpty(ammo) && !isCaptureMode(stack)))
 				{
-					EntitySafariNet esn = new EntitySafariNet(world, player, ammo);
-					world.spawnEntityInWorld(esn);
+					if(!world.isRemote)
+					{
+						EntitySafariNet esn = new EntitySafariNet(world, player, ammo);
+						world.spawnEntityInWorld(esn);
+						
+						world.playSoundAtEntity(player, "random.bow", 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
+					}
+					player.inventory.setInventorySlotContents(i, null);
+					break;
 				}
-				player.inventory.setInventorySlotContents(i, null);
-				break;
 			}
 		}
 		return stack;
+	}
+	
+	private boolean isCaptureMode(ItemStack stack)
+	{
+		return stack != null && stack.getItemDamage() == 1;
 	}
 }
