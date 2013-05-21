44eb19c273cff05451e81b33405680f57ec26ec5
powercrystals <powcrystals@gmail.com>
2013-05-20 23:55:09 -0400

    Added vanilla recipes for new machines

--- src/powercrystals/minefactoryreloaded/setup/recipe/Vanilla.java
+++ src/powercrystals/minefactoryreloaded/setup/recipe/Vanilla.java
@@ -566,6 +566,63 @@ public class Vanilla
 				'M', MineFactoryReloadedCore.machineBaseItem,
 					} ));
 		}
+		
+		if(Machine.BlockSmasher.getIsRecipeEnabled())
+		{
+			GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(MineFactoryReloadedCore.machineBlocks.get(2), 1, 3), new Object[]
+					{
+				"GGG",
+				"HHH",
+				"BMB",
+				'G', "sheetPlastic",
+				'H', MineFactoryReloadedCore.factoryHammerItem,
+				'B', Item.book,
+				'M', MineFactoryReloadedCore.machineBaseItem,
+					} ));
+		}
+		
+		if(Machine.RedNote.getIsRecipeEnabled())
+		{
+			GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(MineFactoryReloadedCore.machineBlocks.get(2), 1, 4), new Object[]
+					{
+				"GGG",
+				"CNC",
+				" M ",
+				'G', "sheetPlastic",
+				'C', MineFactoryReloadedCore.rednetCableBlock,
+				'N', Block.music,
+				'M', MineFactoryReloadedCore.machineBaseItem,
+					} ));
+		}
+		
+		if(Machine.AutoBrewer.getIsRecipeEnabled())
+		{
+			GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(MineFactoryReloadedCore.machineBlocks.get(2), 1, 5), new Object[]
+					{
+				"GGG",
+				"CBC",
+				"RMR",
+				'G', "sheetPlastic",
+				'C', Block.chest,
+				'B', Item.brewingStand,
+				'R', Item.redstoneRepeater,
+				'M', MineFactoryReloadedCore.machineBaseItem,
+					} ));
+		}
+		
+		if(Machine.FruitPicker.getIsRecipeEnabled())
+		{
+			GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(MineFactoryReloadedCore.machineBlocks.get(2), 1, 6), new Object[]
+					{
+				"GGG",
+				"SXS",
+				"SMS",
+				'G', "sheetPlastic",
+				'X', Item.axeGold,
+				'S', Item.shears,
+				'M', MineFactoryReloadedCore.machineBaseItem,
+					} ));
+		}
 	}
 	
 	protected void registerMachineUpgrades()
