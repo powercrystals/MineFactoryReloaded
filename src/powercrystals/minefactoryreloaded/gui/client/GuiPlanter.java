67a831864eb15798713f860448c4f73769658306
Emy <emy@fake.email>
2013-05-26 17:40:53 -0600

    Text position adjustments

--- src/powercrystals/minefactoryreloaded/gui/client/GuiPlanter.java
+++ src/powercrystals/minefactoryreloaded/gui/client/GuiPlanter.java
@@ -1,5 +1,9 @@
 package powercrystals.minefactoryreloaded.gui.client;
 
+import net.minecraft.util.StatCollector;
+
+import org.lwjgl.opengl.GL11;
+
 import powercrystals.minefactoryreloaded.gui.container.ContainerUpgradable;
 import powercrystals.minefactoryreloaded.tile.base.TileEntityFactoryPowered;
 
@@ -14,7 +18,22 @@ public class GuiPlanter extends GuiUpgradable
 	@Override
 	protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY)
 	{
-		fontRenderer.drawString("Filter", 8, 24, 4210752);
-		super.drawGuiContainerForegroundLayer(mouseX, mouseY);
+		fontRenderer.drawString("Filter", 8, 22, 4210752);
+		
+		//GuiFactoryPowered
+		drawBar(140, 75, _tePowered.getEnergyStoredMax(), _tePowered.getEnergyStored(), _barColorEnergy);
+		drawBar(150, 75, _tePowered.getWorkMax(), _tePowered.getWorkDone(), _barColorWork);
+		drawBar(160, 75, _tePowered.getIdleTicksMax(), _tePowered.getIdleTicks(), _barColorIdle);
+		
+		//GuiFactoryInventory, tweaked slightly
+		fontRenderer.drawString(_tileEntity.getInvName(), 8, 6, 4210752);
+		fontRenderer.drawString(StatCollector.translateToLocal("container.inventory"), 8, ySize - 96 + 4, 4210752);
+		
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		if(_tileEntity.getTank() != null && _tileEntity.getTank().getLiquid() != null)
+		{
+			int tankSize = _tileEntity.getTank().getLiquid().amount * _tankSizeMax / _tileEntity.getTank().getCapacity();
+			drawTank(122, 75, _tileEntity.getTank().getLiquid().itemID, _tileEntity.getTank().getLiquid().itemMeta, tankSize);
+		}
 	}
 }
