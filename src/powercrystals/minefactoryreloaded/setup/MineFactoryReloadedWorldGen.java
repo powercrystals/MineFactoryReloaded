e1393fbffbfc0d4d5abe878a17b458d38e90ecf4
powercrystals <powcrystals@gmail.com>
2013-05-26 16:22:54 -0400

    Fix lake worldgen

--- src/powercrystals/minefactoryreloaded/setup/MineFactoryReloadedWorldGen.java
+++ src/powercrystals/minefactoryreloaded/setup/MineFactoryReloadedWorldGen.java
@@ -46,7 +46,7 @@ public class MineFactoryReloadedWorldGen implements IWorldGenerator
 		
 		if(MFRConfig.mfrLakeWorldGen.getBoolean(true) && world.provider.canRespawnHere())
 		{
-			if(random.nextInt(MFRConfig.mfrLakeRarity.getInt()) == 0)
+			if(random.nextInt(MFRConfig.mfrLakeSludgeRarity.getInt()) == 0)
 			{
 				int lakeX = x - 8 + random.nextInt(16);
 				int lakeY = random.nextInt(128);
@@ -54,7 +54,7 @@ public class MineFactoryReloadedWorldGen implements IWorldGenerator
 				new WorldGenLakesMeta(MineFactoryReloadedCore.sludgeLiquid.blockID, 7).generate(world, random, lakeX, lakeY, lakeZ);
 			}
 			
-			if(random.nextInt(MFRConfig.mfrLakeRarity.getInt()) == 0)
+			if(random.nextInt(MFRConfig.mfrLakeSewageRarity.getInt()) == 0)
 			{
 				int lakeX = x - 8 + random.nextInt(16);
 				int lakeY = random.nextInt(128);
