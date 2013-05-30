a31012538a2c2d3421d1a0a5bef6390b3991f107
skyboy <mr.wraith.06@gmail.com>
2013-05-29 21:30:55 -0400

    Drinking lava is no longer a slap on the wrist
    Burp sound effect for obsidian is louder

--- src/powercrystals/minefactoryreloaded/farmables/drinkhandlers/DrinkHandlerLava.java
+++ src/powercrystals/minefactoryreloaded/farmables/drinkhandlers/DrinkHandlerLava.java
@@ -2,6 +2,7 @@ package powercrystals.minefactoryreloaded.farmables.drinkhandlers;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.DamageSource;
 import powercrystals.minefactoryreloaded.api.ILiquidDrinkHandler;
 
 public class DrinkHandlerLava implements ILiquidDrinkHandler
@@ -9,8 +10,20 @@ public class DrinkHandlerLava implements ILiquidDrinkHandler
 	@Override
 	public void onDrink(EntityPlayer player)
 	{
+		player.attackEntityFrom(new InternalLavaDamage(), 7);
 		player.setFire(30);
 		NBTTagCompound tag = player.getEntityData();
 		tag.setLong("drankLavaTime", player.worldObj.getTotalWorldTime());
+	}
+	
+	protected class InternalLavaDamage extends DamageSource
+	{
+		public InternalLavaDamage()
+		{
+			super(DamageSource.lava.damageType);
+			this.setDamageBypassesArmor();
+			this.setFireDamage();
+			this.setDifficultyScaled();
+		}
 	}
 }
