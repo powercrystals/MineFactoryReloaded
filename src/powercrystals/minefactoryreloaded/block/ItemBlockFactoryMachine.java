f5faa6ff255a1e662a0fa3608484a80477e9ef8b
Emy <emy@fake.email>
2013-07-09 11:13:36 -0600

    Revert "BlockFMachine uses new DSU system"
    This reverts commit 0082f6c2076369e35f86623e0b7414256acfd247.

--- src/powercrystals/minefactoryreloaded/block/ItemBlockFactoryMachine.java
+++ src/powercrystals/minefactoryreloaded/block/ItemBlockFactoryMachine.java
@@ -45,20 +45,10 @@ public class ItemBlockFactoryMachine extends ItemBlockFactory
 		NBTTagCompound c = stack.getTagCompound();
 		if(getBlockID() == Machine.DeepStorageUnit.getBlockId() && stack.getItemDamage() == Machine.DeepStorageUnit.getMeta() && c != null)
 		{
-			ItemStack storedItem;
 			int storedId = c.getInteger("storedId");
 			int storedMeta = c.getInteger("storedMeta");
-			int storedQuantity = c.getInteger("storedQuantity");
-			if(storedId != 0 && storedQuantity > 0)
-			{
-				storedItem = new ItemStack(storedId, storedQuantity, storedMeta);
-			}
-			else
-			{
-				storedItem = new ItemStack(0, 0, 0);
-				storedItem.readFromNBT((NBTTagCompound)c.getTag("storedStack"));
-			}
-			info.add("Contains " + storedItem.stackSize + " " + Item.itemsList[storedItem.itemID].getItemDisplayName(storedItem) + " (" + storedItem.itemID + ":" + storedItem.itemID + ")");
+			ItemStack storedItem = new ItemStack(storedId, 1, storedMeta);
+			info.add("Contains " + c.getInteger("storedQuantity") + " " + Item.itemsList[storedId].getItemDisplayName(storedItem) + " (" + storedId + ":" + storedMeta + ")");
 		}
 		else if(getBlockID() == Machine.BioFuelGenerator.getBlockId() && stack.getItemDamage() == Machine.BioFuelGenerator.getMeta())
 		{
